66c9f444ee84a1f4823d1f5a545ffdbfb3f73efe
avenarius <61959278+avenarius-argus@users.noreply.github.com>
2023-12-04 01:32:38 -0500

    node for a6

--- app.js
+++ app.js
@@ -3,19 +3,44 @@ import Hello from './hello.js';
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+
+mongoose.connect(CONNECTION_STRING);
+
 var corsOptions = {
 
     credentials: true,
-    origin: '*'
+    origin: process.env.FRONTEND_URL
+
     
   }
 const app = express()
 app.use(
     cors(corsOptions)
 );  
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
+///3.5.1
 
 Hello(app);
 ModuleRoutes(app);
